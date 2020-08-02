c3887ff80ce1466074b832e1132e64adc9db9061
HDonovan96 <harry.bd11@gmail.com>
2020-08-02 12:19:42 +0100

    Program will now attempt to auto generate tris from points

--- PythonProject.py
+++ PythonProject.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import math
 
 background_colour = (0, 0, 0)
 (width, height) = (800, 600)
@@ -12,12 +13,12 @@ pygame.display.flip()
 
 lineColor = (255, 255, 255)
 vertices = []
-triAreas = []
+triangles = []
 allowInput = True
 
 def GetEvents():
     global vertices
-    global triAreas
+    global triangles
     global allowInput
 
     for event in pygame.event.get():
@@ -29,10 +30,48 @@ def GetEvents():
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 vertices = []
-                triAreas = []
+                triangles = []
                 allowInput = True
             if event.key == pygame.K_c and len(vertices) > 2:
                 allowInput = False
+
+def CalculateTriAreas(vertices):
+    area = vertices[0][0] * (vertices[2][1] - vertices[1][1])
+    area += vertices[1][0] * (vertices[0][1] - vertices[2][1])
+    area += vertices[2][0] * (vertices[1][1] - vertices[0][1])
+
+    return abs(area)
+
+class Triangle:
+    area = 0
+    distToTargetVert = [0] * 2
+    targetVert = [0] * 2
+    def __init__(self, rootVert):
+        self.rootVert = rootVert
+
+    def CalculateArea(self):
+        self.area = CalculateTriAreas((self.rootVert, self.targetVert[0], self.targetVert[1]))
+
+def CalculateTris():
+    global triangles
+
+    i = 0
+    for rootVertex in vertices:
+        triangles.append(Triangle(rootVertex))
+
+        for targetVertex in vertices:
+            if rootVertex != targetVertex:
+                dist = math.sqrt((rootVertex[0] - targetVertex[0]) ** 2 + (rootVertex[1] - targetVertex[1]) ** 2)
+                if dist > triangles[-1].distToTargetVert[0]:
+                    triangles[-1].targetVert[0] = targetVertex
+                    triangles[-1].distToTargetVert[0] = dist
+                elif dist > triangles[-1].distToTargetVert[1]:
+                    triangles[-1].targetVert[1] = targetVertex
+                    triangles[-1].distToTargetVert[1] = dist
+
+        triangles[-1].CalculateArea()
+            
+        i += 1
 
 def DrawShape():
     global vertices
@@ -45,29 +84,25 @@ def DrawShape():
         
         i = i + 1
 
+    for element in triangles:
+        pygame.draw.polygon(screen, (0, 0, 255), (element.rootVert, element.targetVert[0], element.targetVert[1]))
+
     if not allowInput:
         pygame.draw.line(screen, lineColor, vertices[0], vertices[-1])
-
-def CalculateTriAreas(vertices):
-    area = vertices[0][0] * (vertices[2][1] - vertices[1][1])
-    area += vertices[1][0] * (vertices[0][1] - vertices[2][1])
-    area += vertices[2][0] * (vertices[1][1] - vertices[0][1])
-
-    return abs(area)
-
+        CalculateTris()
+        
 def CalculateIfMouseIsInTri():
-    global triAreas
     global vertices
 
     mousePos = pygame.mouse.get_pos()
 
     i = 0
-    for element in triAreas:
-        mouseArea = CalculateTriAreas((mousePos, vertices[i * 3], vertices[i * 3 + 1]))
-        mouseArea += CalculateTriAreas((mousePos, vertices[i * 3], vertices[i * 3 + 2]))
-        mouseArea += CalculateTriAreas((mousePos, vertices[i * 3 + 1], vertices[i * 3 + 2]))
+    for element in triangles:
+        mouseArea = CalculateTriAreas((mousePos, element.rootVert, element.targetVert[0]))
+        mouseArea += CalculateTriAreas((mousePos, element.rootVert, element.targetVert[1]))
+        mouseArea += CalculateTriAreas((mousePos, element.targetVert[0], element.targetVert[1]))
 
-        if element == mouseArea:
+        if element.area == mouseArea:
             return True
 
         i = i + 1
@@ -76,20 +111,12 @@ def CalculateIfMouseIsInTri():
 
 def DoMouseCollision():
     global vertices
-    global triAreas
     global allowInput
 
-    if not allowInput:
-        if len(triAreas) == 0:
-            i = 0
-            while i < int(round(len(vertices) / 3)):
-                triAreas.append(CalculateTriAreas((vertices[i * 3], vertices[i * 3 + 1], vertices[i * 3 + 2])))
-                i += 1
-
-        if CalculateIfMouseIsInTri():
-            pygame.draw.circle(screen, (255, 0, 0), pygame.mouse.get_pos(), 5)
-        else:
-            pygame.draw.circle(screen, (0, 255, 0), pygame.mouse.get_pos(), 5)
+    if CalculateIfMouseIsInTri():
+        pygame.draw.circle(screen, (255, 0, 0), pygame.mouse.get_pos(), 5)
+    else:
+        pygame.draw.circle(screen, (0, 255, 0), pygame.mouse.get_pos(), 5)
 
 running = True
 while running:
@@ -97,7 +124,8 @@ while running:
 
     GetEvents()
     DrawShape()
-    DoMouseCollision()
+    if not allowInput:
+        DoMouseCollision()
 
     pygame.display.update()
 
